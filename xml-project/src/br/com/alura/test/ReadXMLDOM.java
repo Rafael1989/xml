f2cd5fc5f6335ea6fc16737d259f7b6a7555aed7
Rafael <Rafael@DESKTOP-CUAPP9L>
2017-12-25 21:28:29 -0200

    Trabalhando com XPath

--- xml-project/src/br/com/alura/test/ReadXMLDOM.java
+++ xml-project/src/br/com/alura/test/ReadXMLDOM.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -14,7 +19,7 @@ import org.xml.sax.SAXException;
 import br.com.alura.model.Produto;
 
 public class ReadXMLDOM {
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
 		DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
 		fabrica.setValidating(true);
 		fabrica.setNamespaceAware(true);
@@ -26,7 +31,10 @@ public class ReadXMLDOM {
 		System.out.println(moeda);
 		NodeList element = document.getElementsByTagName("formaDePagamento");
 		System.out.println(element.item(0).getTextContent());
-		NodeList produtos = document.getElementsByTagName("produto");
+		String expressao = "/venda/produtos/produto[3]";
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		XPathExpression xPathExpression = xPath.compile(expressao);
+		NodeList produtos = (NodeList) xPathExpression.evaluate(document, XPathConstants.NODESET);
 		for(int i = 0;i < produtos.getLength();i++) {
 			Element produto = (Element)produtos.item(i);
 			Produto prod = new Produto(produto.getElementsByTagName("nome").item(0).getTextContent(), Double.parseDouble(produto.getElementsByTagName("preco").item(0).getTextContent()));
