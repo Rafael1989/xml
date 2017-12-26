707ac1a63eb33a323f22242b4482ae8b8c4923b0
Rafael <Rafael@DESKTOP-CUAPP9L>
2017-12-26 00:27:14 -0200

    Convertendo object to xml com Jaxb

--- xml-project/src/br/com/alura/test/XMLToObjetJaxb.java
+++ xml-project/src/br/com/alura/test/XMLToObjetJaxb.java
@@ -1,11 +1,15 @@
 package br.com.alura.test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import br.com.alura.model.Produto;
 import br.com.alura.model.Venda;
 
 public class XMLToObjetJaxb {
@@ -15,5 +19,18 @@ public class XMLToObjetJaxb {
 		Venda venda = (Venda) unmarshaller.unmarshal(new File("src/venda.xml"));
 		
 		System.out.println(venda);
+		
+		Venda venda2 = new Venda();
+		
+		venda2.setFormaDePagamento("Débito");
+		List<Produto> produtos = new ArrayList<>();
+		Produto produto = new Produto();
+		produto.setNome("Tênis");
+		produto.setPreco(24.69);
+		produtos.add(produto);
+		venda2.setProdutos(produtos);
+		
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.marshal(venda2, new File("src/xmlGerado.xml"));
 	}
 }
